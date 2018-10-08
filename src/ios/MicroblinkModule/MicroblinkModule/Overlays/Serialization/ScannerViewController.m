c0f02c04ac892949010f09de6641e7a10225c6d9
Pavel Vilbik <p.vilbik@softteco.com>
2018-10-08 17:19:03 +0300

    add custom documnet scanner view

---
+++ src/ios/MicroblinkModule/MicroblinkModule/Overlays/Serialization/ScannerViewController.m
@@ -0,0 +1,41 @@
+//
+//  ScannerViewController.m
+//  pdf417-sample-Swift
+//
+//  Created by Pavel Vilbik on 10/8/18.
+//  Copyright © 2018 Dino. All rights reserved.
+//
+
+#import "ScannerViewController.h"
+
+@interface ScannerViewController ()
+@property (weak, nonatomic) IBOutlet UIView *borderView;
+
+@end
+
+@implementation ScannerViewController
+
++ (instancetype)initFromStoryboardWith {
+    NSString *path = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"MicroblinkModuleResources.bundle/"];
+    ScannerViewController *scanner = (ScannerViewController *)[[UIStoryboard storyboardWithName:@"Scanner" bundle:[NSBundle bundleWithPath:path]] instantiateInitialViewController];
+    return scanner;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.borderView.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.borderView.layer.borderWidth = 3.0;
+    
+    super.scanningRecognizerRunnerViewControllerDelegate = self;
+}
+
+- (void)recognizerRunnerViewController:(UIViewController<MBRecognizerRunnerViewController> *)recognizerRunnerViewController didFinishScanningWithState:(MBRecognizerResultState)state {
+    
+    [self.delegate overlayViewControllerDidFinishScanning:self state:state];
+}
+
+- (IBAction)cancelPressed:(id)sender {
+    [self.delegate overlayDidTapClose:self];
+}
+
+@end
